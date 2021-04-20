a3e2a033fac042ff93cc506262ec6efdcb694c5c
joel-zilliqa <joel@zilliqa.com>
2021-04-20 05:11:56 +0000

    Updated run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker run polynetwork/zilliqa-relayer /app/run.sh
+docker run -d \
+-v $(pwd)/persistence:/app/persistence \
+-v $(pwd)/secrets/config.local.yaml:/app/config.local.yaml \
+-v $(pwd)/secrets/target_contracts.json:/app/target_contracts.json \
+-v $(pwd)/secrets/poly.wallet:/app/poly.wallet \
+-v $(pwd)/secrets/zilliqa.wallet:/app/zilliqa.wallet \
+polynetwork/zilliqa-relayer 
